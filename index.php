3b1c1f48d57f704a39ef979242111512db7ade8a
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-14 21:45:39 +0200

    connectted to swl server

--- index.php
+++ index.php
@@ -7,8 +7,33 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    echo("labas");
-    ?>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "mysql";
+$dbname = "sprint1";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = 'SELECT id, name, last_name, age, project_id FROM sprint1.darbuotojai';
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["last_name"]." " . $row["age"]." " . $row["project_id"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
+
 </body>
 </html>
