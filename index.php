9086526d308c909f8fc731fd46b5e6d5e5a0fb83
Justas383 <74533002+Justas383@users.noreply.github.com>
2021-03-16 18:55:09 +0200

    showing both DB

--- index.php
+++ index.php
@@ -4,11 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="main">
+<table>
+<tr>
+<td>Id</td>
+<td>name</td>
+<td>Last name</td>
+<td>age</td>
+<td>project name</td>
+</tr>
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "mysql";
@@ -21,19 +30,48 @@ if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-$sql = 'SELECT id, name, last_name, age, project_id FROM sprint1.darbuotojai';
+$sql = 'SELECT id, name, last_name, age, project_name FROM sprint1.darbuotojai';
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["last_name"]." " . $row["age"]." " . $row["project_id"]. "<br>";
+        print("<tr><td> " . $row["id"]."</td> " .
+              " <td>" . $row["name"]."</td> " .
+               "<td>" . $row["last_name"]."</td> " .
+               "<td>" . $row["age"]."</td> " .
+               "<td>" . $row["project_name"]."</td><tr> ");
     }
-} else {
+}
+ else {
     echo "0 results";
+    
+}
+?>
+<tr>
+<td>Project id</td>
+<td>Project name</td>
+
+</tr>
+<?php
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = 'SELECT project_id, project_name FROM sprint1.projektai';
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        print("<tr><td> " . $row["project_id"]."</td> " .
+              " <td>" . $row["project_name"]."</td><tr> ");
+    }
 }
 
 mysqli_close($conn);
 ?>
-
+</table>
+</div>
 </body>
 </html>
